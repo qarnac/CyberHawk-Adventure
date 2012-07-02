ea2ae1b30fa487c69d218c7acb7fb10f56c7d671
sabareesh kkanan <sabareeshkkanan@gmail.com>
2012-07-02 16:30:58 -0700

    Java script Spliced into two main.js and activity.js

--- quest/admin/js/activity.js
+++ quest/admin/js/activity.js
@@ -1,64 +1,7 @@
-/*
- * Common scripts needed
- * handles ajax
- * handles selection of a hunt activity
- * Creates activitiy array
- * USED by welcome.php
- * Wherever you see variable x it is a temporary varibale used for multiple operations
+/**
+ * @author sabareesh kkanan subramani
  */
-
 var hunts;// All the hunts from the teacher will be stored in this variable
-
-//invoked by ajax function but loaded when the page is finished loading. THis receives the hunts from the server and it transfers it to javascript variable hunts
-function init(x) {
-	hunts = JSON.parse(x);
-	$('username').innerHTML=hunts[0];
-	hunts=hunts[1];
-	for ( x = 0; x < hunts.length; x++)
-		$('selecthunt').options[$('selecthunt').options.length] = new Option(hunts[x]['title'], x);
-}
-//Creates some random number
-var uniq = Math.floor((Math.random() * 100) + 1);
-//shortcut to get object with their id
-function $(x) {
-	return document.getElementById(x);
-}
-
-//ajax POST request
-function ajax(data, url, callback) {
-	var xmlhttp;
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp = new XMLHttpRequest();
-	} else {// code for IE6, IE5
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			if (xmlhttp.responseText == 'sessionfail')
-				window.location = '../';
-			callback(xmlhttp.responseText);
-		}
-	}
-	xmlhttp.open("POST", url, true);
-	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xmlhttp.send(data);
-}
-
-
-	//this function invoked when student selects a hunt
-	function huntsel(x) {
-		if (x != 'null') {
-			var hunt = hunts[x];
-			ajax("what=activities&id=" + hunt['id'], 'retrive.php', create_activity_obj);
-
-		} else {
-			$('activity').innerHTML = '';
-			$('students').innerHTML = '';
-			feed = {};
-			activities = [];
-		}
-
-	}
 
 var activities = new Array();
 // Holds all the activities of a particular hunt organised by student id
@@ -210,28 +153,4 @@ function displayactivity(x) {
 	div.appendChild(document.createElement('hr'));
 	//Append the div element to the div 'activity'
 	$('activity').appendChild(div);
-}
-
-//creates a dom element
-function createElement(x, y) {
-	x = document.createElement(x);
-	x.innerHTML = y;
-	return x;
-}
-
-// a custom prototype thats been added to array object to find existence of particular value
-Array.prototype.has = function(v) {
-	for ( i = 0; i < this.length; i++) {
-		if (this[i] == v)
-			return true;
-	}
-	return false;
-}
-//A custom prototype verifies whether paticular id exist inside the array
-Array.prototype.hassid = function(v) {
-	for ( i = 0; i < this.length; i++) {
-		if (this[i].sid == v)
-			return i;
-	}
-	return "false";
 }
