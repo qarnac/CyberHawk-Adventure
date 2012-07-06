15015c3c080b02ef566f145f590dd2f9dd438cac
sabareesh kkanan <sabareeshkkanan@gmail.com>
2012-07-06 14:38:22 -0700

    More modular

--- quest/admin/upload.php
+++ quest/admin/upload.php
@@ -8,16 +8,20 @@ session_start();
 //scripts starts its execution from here by verifying the post request it received and also the session of the user
 if (isset($_SESSION['who']) == 'teacher') {
 	if (isset($_POST['content'])) {
-		$content = json_decode($_POST['content']);
-		$x = array_keys(get_object_vars($content));
-		for ($i = 0; $i < count($x); $i++)
-			query("UPDATE stud_activity SET status='" . esc($content -> $x[$i] -> grant) . "',comments='" . esc($content -> $x[$i] -> comment) . "' WHERE id='" . $x[$i] . "' ");
-		echo "sucess";
-	}
-	else  //if there is unexpected request from the client 
-	echo "unexpectedrequest";
-} else {//if session doesnt exist this commands client to redirect to loginpage 
+		process($_POST['content']);
+	} else//if there is unexpected request from the client
+		echo "unexpectedrequest";
+} else {//if session doesnt exist this commands client to redirect to loginpage
 	echo "sessionfail";
+}
+
+//Processes the data sent from the client to upload it to the Database
+function process($x) {
+	$content = json_decode(x);
+	$x = array_keys(get_object_vars($content));
+	for ($i = 0; $i < count($x); $i++)
+		query("UPDATE stud_activity SET status='" . esc($content -> $x[$i] -> grant) . "',comments='" . esc($content -> $x[$i] -> comment) . "' WHERE id='" . $x[$i] . "' ");
+	echo "sucess";
 }
 
 //dbase query executer
