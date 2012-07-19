4623ffe46e1a1c23140319f9f48f1da2fafa340d
Jeffrey Rackauckas <Jeffreyrack@gmail.com>
2012-07-19 07:52:01 -0700

    Deleting upload.php and retrive.php.

--- quest/php/teacher_retreive.php
+++ quest/php/teacher_retreive.php
@@ -5,6 +5,8 @@
  *Used by Ajax request from wscript.js .
  *Returns Json string to the Client
  */
+ // The if statement is needed solely because user_retrieve.php is used incorrectly at some point that I can not find, and so it redirects here.
+ // When the statement that calls user_retrieve instead of teacher_retrieve is fixed, this if can be removed.
 if (!isset($_SESSION)) session_start();
 if (isset($_SESSION['login']) == true && $_SESSION['who'] == 'teacher') {
 	include '../php/credentials.php';
@@ -17,9 +19,9 @@ if (isset($_SESSION['login']) == true && $_SESSION['who'] == 'teacher') {
 } else {
 	echo "sessionfail";
 }
-
+// Queries the database for the list of student activities, and echos them.
 function activities($x) {
-	$studentactivities = query("SELECT stud_activity.*,students.firstname,students.lastname FROM stud_activity,students WHERE stud_activity.hunt_id='" . mysql_escape_string($x) . "' AND students.id=stud_activity.student_id");
+	$studentactivities = mysql_query("SELECT stud_activity.*,students.firstname,students.lastname FROM stud_activity,students WHERE stud_activity.hunt_id='" . mysql_escape_string($x) . "' AND students.id=stud_activity.student_id") or die(mysql_error());
 	if (mysql_num_rows($studentactivities) == 0) { //checks atleast for one student activity 
 		echo "none";
 	} else {
@@ -31,10 +33,10 @@ function activities($x) {
 	}
 }
 
-//IF Session is valid this returns the username along with the hunts created by the teacher where the status of them is open
+// Finds all open hunts where tid==Session[id].  Then echos result.
 function hunts() {
 	$hunts = array();
-	$result = query("SELECT * FROM hunt WHERE tid='" . $_SESSION['id'] . "' AND status='open'");
+	$result = mysql_query("SELECT * FROM hunt WHERE tid='" . $_SESSION['id'] . "' AND status='open'") or die(mysql_error());
 	if (mysql_num_rows($result) > 0) {
 		while ($x = mysql_fetch_assoc($result)) {
 			array_push($hunts, $x);
@@ -44,11 +46,5 @@ function hunts() {
 	$temp[1] = $hunts;
 	$temp = json_encode($temp);
 	echo $temp;
-}
-
-// Executes the mysql query
-function query($x) {
-	$result = mysql_query($x) or die("mysqlfailed");
-	return $result;
 }
 ?>
