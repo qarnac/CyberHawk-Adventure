ca625f2b0681d21c8d789fa7b49c33119dd1cb04
Jeffrey Rackauckas <jeffreyrack@gmail.com>
2013-05-17 14:29:45 -0700

    Sidemap now properly relocates when hawk moves.

--- js/SideMap.js
+++ js/SideMap.js
@@ -2,8 +2,10 @@
 // topMap is the google map object being passed to this function.
 // I really have no idea what all of the values being set to for the sideMap are
 // used for.
+var jMap;
 function SideMap(topMap) {
 	var me = this;
+	jMap=me;
 	me.imagepath = "images/hawks/car_left_";
 
 	me.topMap = topMap;
@@ -24,7 +26,7 @@ function SideMap(topMap) {
 
 	topMap.setCenter(new google.maps.LatLng(me.centLat, me.centerLong));
 	topMap.setZoom(10);
-	new google.maps.Marker({
+	me.carMarker=new google.maps.Marker({
       position: new google.maps.LatLng(me.centLat, me.centerLong),
       map: me.topMap,
       icon: me.imagepath + "0.png"
@@ -54,7 +56,7 @@ SideMap.prototype.refresh = function(centLat, centerLong, centerAlt, heading )
         
         if(me.loopCounter % 10 == 0)
         {
-            me.topMap.setCenter(new GLatLng(me.centLat, me.centerLong), me.mapLevel); 
+            me.topMap.setCenter(new google.maps.LatLng(me.centLat, me.centerLong), me.mapLevel); 
             
 			// Do this because the way we generate the image headings are backwards.
             me.heading = me.heading * -1; 
@@ -65,12 +67,12 @@ SideMap.prototype.refresh = function(centLat, centerLong, centerAlt, heading )
             var closestHeadingDirection = Math.round(me.heading / 10)*10;
             var locationOfFile = me.imagepath + closestHeadingDirection + ".png";
             //if(!reachedPlace) { alert("Reached line" + locationOfFile); reachedPlace = true; }
-            me.carMarker.setImage(locationOfFile);
+            me.carMarker.setIcon(locationOfFile);
             
             // We subtract .001 from the location for the top view car marker because the Longitude on the top view isn't exactly lined
             // up with the marker locations on the 3D view. The .001 accounts for that discrepecny. -- Added by Paul
-            var point = new GLatLng(me.centLat,me.centerLong - .001);
-            me.carMarker.setLatLng(point);
+            var point = new google.maps.LatLng(me.centLat,me.centerLong - .001);
+            me.carMarker.setPosition(point);
             //var point = new GLatLng(centLat,centerLong);
             //var movingMarker = new GMarker(point, markerOptions);
             //topMap.addOverlay(movingMarker);
